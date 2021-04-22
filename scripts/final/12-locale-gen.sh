2be1dba0a2d7b5cfa48ffa6e00cb85f0ece71405
Aitor Iturrioz <riturrioz@gmail.com>
2021-04-22 08:05:26 +0200

    Check if locale has been set for the first time (#16)
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- scripts/final/12-locale-gen.sh
+++ scripts/final/12-locale-gen.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 {{ if .Data.system.lang }}
-if ! locale | grep LANG={{ .Data.system.lang }}; then
+if locale 2>&1 | grep -q "Cannot"; then
+    echo -n "* Defining locale for the first time"
+    /usr/sbin/locale-gen
+    echo "done"
+elif ! locale | grep LANG={{ .Data.system.lang }}; then
     echo -n "* Changing locale (System language is different than the configured one)"
     /usr/sbin/locale-gen
     echo "done"
